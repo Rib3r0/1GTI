c31d5db6a2c9e0b39069bddbe4490cd2daa9a57c
AlunoTI <eduardorpimentaa@gmail.com>
2024-02-20 10:46:47 -0300

    20_02_24

---
+++ Exercicio_000/src/exercicio_000/Exercicio_000.java
@@ -0,0 +1,11 @@
+package exercicio_000;
+
+
+public class Exercicio_000 {
+
+    public static void main(String[] args) {
+        System.out.println("Eduardo Ribeiro Pimenta");
+        // TODO code application logic here
+    }
+    
+}
